7926aa15b929f36accc67e65cfac73cb3444d674
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2022-12-05 19:08:15 -0300

    primer commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -14,7 +14,7 @@ const Card = ({ name, username, id }) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        <button onClick={addFav} className="favButton">Addaa fav</button>
     </div>
   );
 };
